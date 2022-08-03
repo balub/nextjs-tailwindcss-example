b78be0a36c587a3c54a2e54d5ad9d90aa77f4b0f
egg <rtg@ff.com>
2022-08-04 00:39:37 +0530

    setup the home page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["flagcdn.com","upload.wikimedia.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "hsl(209, 23%, 22%)",
+        veryDarkBlue: "hsl(207, 26%, 17%)",
+        veryLightBlue: "hsl(200, 15%, 8%)",
+        darkGray: "hsl(0, 0%, 52%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
